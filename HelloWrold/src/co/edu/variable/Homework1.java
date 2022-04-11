2e5ae01cc46de187e0c451721f4fabad7caef005
lge <lge@DESKTOP-OEFLRVL>
2022-04-12 00:36:55 +0900

    숙제했음

--- HelloWrold/src/co/edu/variable/Homework1.java
+++ HelloWrold/src/co/edu/variable/Homework1.java
@@ -1,5 +1,7 @@
 package co.edu.variable;
 
+import java.util.Scanner;
+
 public class Homework1 {
 
 	public static void main(String[] args) {
@@ -8,6 +10,26 @@ public class Homework1 {
 		//10개씩 담을 수 있는 상자
 		//5개씩 담을 수 있는 상자
 		//10개씩 담을 수 있는 상자 ???개,
+		
+		Scanner scn = new Scanner(System.in);
+		
+		System.out.print("오렌지 개수를 입력하세요");
+		int orange, result, box1, box2;
+		orange = scn.nextInt();
+		result = 0;
+		box1 = 10;
+		box2 = 5;
+		
+		result = orange / box1;
+		if (result > 0) {
+			System.out.println("10개씩 담을 수 있는 상자 : " + result + "개");
+		}
+		
+		orange = orange % box1;
+		result = orange / box2;
+		if (result > 0) {
+			System.out.println("5개씩 담을 수 있는 상자 : " + result + "개");
+		}
 
 	}
 
--- HelloWrold/src/co/edu/variable/Homework2.java
+++ HelloWrold/src/co/edu/variable/Homework2.java
@@ -1,5 +1,7 @@
 package co.edu.variable;
 
+import java.util.Scanner;
+
 public class Homework2 {
 
 	public static void main(String[] args) {
@@ -10,6 +12,28 @@ public class Homework2 {
 		//[물건가격] 7500원
 		//[부가세] 750원
 		//[잔돈] 2500원
+		
+		Scanner scn = new Scanner(System.in);
+		
+		System.out.println("현금 금액을 입력하세요");
+		int money = scn.nextInt();
+		System.out.println("물건 가격을 입력하세요");
+		int product = scn.nextInt();
+		int result = 0;
+		double surtax;
+		
+		result = money - product;
+		if (result > 0)
+		{
+			System.out.println("잔돈은 " + result + "입니다.");
+		}
+
+		surtax = product / 1.1;
+		result = (int) (product - surtax);
+		if (result > 0)
+		{
+			System.out.println("부가세는 " + result + "입니다.");
+		}
 
 	}
 

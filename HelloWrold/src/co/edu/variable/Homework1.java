f448fd4208acf4b2430ba9b3269452f2b745c24c
admin <admin@YD2-08>
2022-04-11 17:55:34 +0900

    숙제하기

---
+++ HelloWrold/src/co/edu/variable/Homework1.java
@@ -0,0 +1,14 @@
+package co.edu.variable;
+
+public class Homework1 {
+
+	public static void main(String[] args) {
+		
+		//오렌지 127개
+		//10개씩 담을 수 있는 상자
+		//5개씩 담을 수 있는 상자
+		//10개씩 담을 수 있는 상자 ???개,
+
+	}
+
+}
---
+++ HelloWrold/src/co/edu/variable/Homework2.java
@@ -0,0 +1,16 @@
+package co.edu.variable;
+
+public class Homework2 {
+
+	public static void main(String[] args) {
+		
+		//영수증
+		//물건의 가격과 현금
+		//[현금] 10000원
+		//[물건가격] 7500원
+		//[부가세] 750원
+		//[잔돈] 2500원
+
+	}
+
+}

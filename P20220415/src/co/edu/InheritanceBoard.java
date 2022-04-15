826ab0a084c4c77d75b1487b2352627344d48858
admin <admin@YD2-08>
2022-04-15 17:56:18 +0900

    상속 @Override

---
+++ P20220415/src/co/edu/InheritanceBoard.java
@@ -0,0 +1,87 @@
+package co.edu;
+
+//-------------------상속 했음 밑에 @Override-----------------
+
+public class InheritanceBoard {
+
+	//게시글번호, 제목, 내용, 작성자, 조회회수(조회할때마다 +1증가)
+	//필드
+	private int boardNo;
+	private String title;
+	private String content;
+	private String writer;
+	private int searchCnt = 0;
+	
+	//생성자
+	public InheritanceBoard(int boardNo, String title, String content, String writer) {
+		this.boardNo = boardNo;
+		this.title = title;
+		this.content = content;
+		this.writer = writer;
+	}
+
+	//메소드
+	public int getBoardNo() {
+		return boardNo;
+	}
+	public void setBoardNo(int boardNo) {
+		this.boardNo = boardNo;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+
+	public int getSearchCnt() {
+		return searchCnt;
+	}
+	public void setSearchCnt(int searchCnt) {
+		this.searchCnt = searchCnt;
+	}
+	
+
+	public void getInfo() {
+		//게시물번호 제목 내용 작성자 조회수
+		System.out.printf("%3d %-15s %-15s %-5s %2d\n",
+				this.boardNo, this.title, this.content, this.writer, this.searchCnt);
+	}
+	
+	public void getDetailInfo() {
+		//게시글번호		작성자
+		//제목			조회수
+		//내용
+		String info ="=============================================\n" +
+					"게시글 번호 : %2d			작성자 : %-5s\n" +
+					"제목 : %-10s				조회수 : %3d\n" +
+					"내용 : %-30s\n" +
+					"==============================================\n";
+		System.out.printf(info,
+				this.boardNo, this.writer, this.title, this.searchCnt, this.content);
+	}
+
+	@Override	//부모클래스 toStirng -> 자실클래스에서 재정의, 부모클래스메소드를 자식클래스로 새롭게 불러온다 
+	public String toString() {
+		return "Board [boardNo=" + boardNo + ", title=" + title + ", content=" + content + ", writer=" + writer
+				+ ", searchCnt=" + searchCnt + "]";
+	}
+	
+	
+	
+}

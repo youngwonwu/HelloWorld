83d5e35ccc5fb2f9ea636f9063813a211c88f72f
lge <lge@DESKTOP-OEFLRVL>
2022-04-25 21:38:51 +0900

    추상클래스

--- P20220418/src/co/edu/abstracts/Main.java
+++ P20220418/src/co/edu/abstracts/Main.java
@@ -4,7 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Animal animal = null; //new Animal(); -> X, 추상크래스를 받을려면 재정의 해야한다
+		// 추상클래스는 생성자 호출 못함
+		Animal animal = null; //new Animal(); -> X, 추상클래스를 받을려면 재정의 해야한다
+		
 		animal = new Bird();
 		animal.eat();
 		animal.run();

1e08b454875cadd72880f7e73052e448f1437c2b
lge <lge@DESKTOP-OEFLRVL>
2022-04-25 20:45:56 +0900

    static
    final

--- P20220415/src/co/deu/statics/PersonExample.java
+++ P20220415/src/co/deu/statics/PersonExample.java
@@ -8,7 +8,7 @@ public class PersonExample {
 		p1.name = "김길동";
 		//p1.ssn = "123345-567534";	//ssn은 final 이여서 이미 위에서 ssn에 값을 넣어 변경이 안됨
 		
-		Person p2 = new Person("345567-123345", "김길동");	//새로운 인스턴스..???
+		Person p2 = new Person("345567-123345", "김길동");	//새로운 인스턴스
 
 	}
 
